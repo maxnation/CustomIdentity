7d1c549aa9195817da417c936e8f6cde3e79853f
Maxim <maksim.torba@nixsolutions.com>
2020-05-04 11:34:11 +0300

    dbContextAdded

---
+++ CustomIdentity/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using CustomORM;
+
+namespace CustomIdentity.Data
+{
+    public class ApplicationDbContext : ContextBase
+    {
+        public Repository<ApplicationUser> Users { get; set; }
+
+        public ApplicationDbContext() : base("DefaultConnection")
+        {
+        }
+    }
+}
