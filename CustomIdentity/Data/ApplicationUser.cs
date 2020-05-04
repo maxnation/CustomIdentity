17dac84eb7212fcad7ce46380080b435ef43545d
Maxim <maksim.torba@nixsolutions.com>
2020-05-04 11:15:17 +0300

    Application User added

---
+++ CustomIdentity/Data/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿namespace CustomIdentity.Data
+{
+    public class ApplicationUser : IUser
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string NormalizedUserName { get; set; }
+
+        public string Password { get; set; }
+    }
+}
---
+++ CustomIdentity/Data/IUser.cs
@@ -0,0 +1,13 @@
+﻿namespace CustomIdentity.Data
+{
+    public interface IUser
+    {
+        int Id { get; set; }
+
+        string Username { get; set; }
+
+        string NormalizedUserName { get; set; }
+
+        string Password { get; set; }
+    }
+}
