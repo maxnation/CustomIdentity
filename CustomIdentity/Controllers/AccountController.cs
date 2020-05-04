4548c4b2831024120e066a5a046ae15109bf4597
Maxim <maksim.torba@nixsolutions.com>
2020-05-04 12:26:35 +0300

    Realization of AccountController

---
+++ CustomIdentity/Controllers/AccountController.cs
@@ -0,0 +1,57 @@
+﻿using System.Threading.Tasks;
+using CustomIdentity.Data;
+using CustomIdentity.Models;
+using MediaBrowser.Controller.Net;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomIdentity.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public AccountController(SignInManager<ApplicationUser> signInManager)
+        {
+            this.signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await this.signInManager.PasswordSignInAsync(model.Username, model.Password, false, false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Неправильный логин и (или) пароль");
+                }
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        [Authenticated]
+        public async Task<IActionResult> Logout()
+        {
+            await this.signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
