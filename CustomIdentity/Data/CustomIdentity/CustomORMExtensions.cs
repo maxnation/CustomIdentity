5278062f80424048cd8a2774a7af1b15b260c47e
Maxim <maksim.torba@nixsolutions.com>
2020-05-04 11:48:04 +0300

    Added ORMExtensions class to get user by name

---
+++ CustomIdentity/Data/CustomIdentity/CustomORMExtensions.cs
@@ -0,0 +1,13 @@
+﻿using CustomORM;
+using System.Linq;
+
+namespace CustomIdentity.Data.CustomIdentity
+{
+    public static class CustomORMExtensions
+    {
+        public static IUser FindUserByName<TUser>(this Repository<TUser> repository, string username) where TUser : IUser
+        {
+            return repository.GetAll().FirstOrDefault(u => u.Username == username);
+        }
+    }
+}
