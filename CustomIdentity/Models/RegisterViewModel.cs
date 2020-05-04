31b933aad83c9408108df9a3ebe7dc2dbb263c75
Maxim <maksim.torba@nixsolutions.com>
2020-05-04 21:47:56 +0300

    added registerViewModel

---
+++ CustomIdentity/Models/RegisterViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomIdentity.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        [Required]
+        public string PasswordConfirm { get; set; }
+    }
+}
