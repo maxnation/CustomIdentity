9432a839d2a07959e52db1f2f7738ae71e1b6190
Maxim <maksim.torba@nixsolutions.com>
2020-05-04 12:19:56 +0300

    Added LoginViewModel

---
+++ CustomIdentity/Models/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomIdentity.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required]
+        public string Password { get; set; }
+    }
+}
